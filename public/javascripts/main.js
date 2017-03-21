ac16b5f94963740824e6e0e59d6dde5da013045c
zm <maratn566ek@gmail.com>
2017-03-22 00:55:11 +0300

    1

---
+++ public/javascripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+	// $('#registration').click((event) => {
+	// 	$('html').load('registration');
+	// 	// $.ajax({
+	// 	// 	url: '/registration',
+	// 	// 	type: 'GET',
+	// 	// 	// dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+	// 	// 	// data: {param1: 'value1'},
+	// 	// })
+	// 	// .done(function() {
+	// 	// 	console.log("success");
+	// 	// 	$('html').load('registration');
+	// 	// })
+	// 	// .fail(function() {
+	// 	// 	console.log("error");
+	// 	// })
+	// 	// .always(function() {
+	// 	// 	console.log("complete");
+	// 	// });
+		
+	// });
+});
