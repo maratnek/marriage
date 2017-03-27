359f9443df49a6ec0e70c03eb7cda2372ed4fc64
maratnek <maratn566ek@gmail.com>
2017-03-27 18:46:26 +0300

    add filter && revision finder

--- models/user.js
+++ models/user.js
@@ -4,29 +4,44 @@ const config = require('../config/database');
 
 // User Schema
 const UserSchema = mongoose.Schema({
-	name: {
-		type: String
-	},
-	surname: {
-		type: String,
-		required: true
-	},
-	fathname: {
-		type: String,
-		required: true
-	},
-	sex: {
-		type: String,
-		required: true
-	},
-	religion: {
-		type: String,
-		required: true
-	},
-	date: {
-		type: Date,
-		required: true
-	}
+	name: 									{		type: String,		required: true	},
+	surname: 								{		type: String,		required: true	},
+	fathname: 							{		type: String,		required: true	},
+	email: 									{		type: String,			},
+	sex: 										{		type: String,		required: true	},
+	religion: 							{		type: String,		required: true	},
+	date: 									{		type: Date,		required: true	},
+	nation: 								{		type: String,		required: true	},
+	telephone: 							{		type: Number,		required: true	},
+	size: 									{		type: Number,		required: true	},
+	weight: 								{		type: Number,		required: true	},
+	marriage: 							{		type: String,		required: true	},
+	children: 							{		type: Number,		required: true	},
+	live_place: 						{		type: String,		required: true	},
+	svet_edication: 				{		type: String,		required: true	},
+	profession:							{		type: String,		required: true	},
+	work_place:							{		type: String,		required: true	},
+	religion_edication:			{		type: String,		required: true	},
+	observing:							{		type: String,		required: true	},
+	attribute:							{		type: String,		required: true	},
+	character:							{		type: String,		required: true	},
+	hobby:									{		type: String,		required: true	},
+	old_min: 								{		type: String,		required: true	},
+	old_max: 								{		type: String,		required: true	},
+	religion2: 							{		type: String,		required: true	},
+	nation2: 								{		type: String	},
+	size2_min: 							{		type: Number,		required: true	},
+	size2_max: 							{		type: Number,		required: true	},
+	weight2_min:						{		type: Number,		required: true	},
+	weight2_max:						{		type: Number,		required: true	},
+	marriage2: 							{		type: String,		required: true	},
+	children2: 							{		type: Number,		required: true	},
+	svet_edication2: 				{		type: String			},
+	religion_edication2:		{		type: String			},
+	observing2:							{		type: String			},
+	attribute2:							{		type: String			},
+	character2:							{		type: String			},
+	dopoln:									{		type: String			}
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,20 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Marriage' });
+  res.render('index', { title: 'Никахлашу' });
 });
 
 // date format in YYYY-MM-DD
-var dateFormat = (str_date) => {
-	var dl = new Date(str_date);
-	return dl.getFullYear()-12 + '-' 
-				+ ((dl.getMonth()+1)<10 ? '0' + (dl.getMonth()+1) : (dl.getMonth()+1))  + '-' 
-				+ dl.getDate();
+var dateFormat = (date) => {
+	return date.toLocaleString('en-US', 
+		{timeZone:'UTC',year:'numeric', month:'numeric', day:'numeric'});
+};
 
-}
+function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
 
 router.get('/registration', function(req, res, next) {
 	// var dl = new Date();
@@ -30,15 +33,11 @@ router.get('/persons', function(req, res, next) {
 	User.getUserAll((err, user)=>{
 		if (err) return console.log(err);
 		console.log(user);
-  	res.render('persons', { title: 'Persons',  person: user.map((it,index)=>{
-  		var p = it;
+  	res.render('persons', { title: 'Анкеты участников',  person: user.map((it,index)=>{
   		date = new Date(it.date);
-  		p.yearsOld = date - Date();
-  		console.log(p.yearsOld);
-  		p.dateNew = dateFormat(it.date);
-  		console.log(dateFormat(it.date));
-  		console.log(date.toDateString());
-  		return p;	
+  		it.yearsOld = calculateAge(date);
+  		it.dateNew = dateFormat(date);
+  		return it;	
   	}) });
 	});
 });
@@ -46,13 +45,46 @@ router.get('/persons', function(req, res, next) {
 // Register
 router.post('/registration', (req,res,next)=>{
 	console.log('new user add');
+	console.log(req.body);
 	var newUser = new User({
-		name: req.body.name,
-		surname: req.body.surname,
-		fathname: req.body.fathname,
-		sex: req.body.sex,
-		date: req.body.date,
-		religion: req.body.religion
+		name: 									req.body.name,
+		surname: 								req.body.surname,
+		fathname: 							req.body.fathname,
+		email: 									req.body.email,
+		sex: 										req.body.sex,
+		date: 									req.body.date,
+		religion: 							req.body.religion,
+		nation: 								req.body.nation,
+		telephone: 							req.body.telephone,
+		size: 									req.body.size,
+		weight: 								req.body.weight,
+		marriage: 							req.body.marriage,
+		children: 							req.body.children,
+		live_place: 						req.body.live_place,
+		svet_edication: 				req.body.svet_edication,
+		profession:							req.body.profession,
+		work_place:							req.body.work_place,
+		religion_edication:			req.body.religion_edication,
+		observing:							req.body.observing,
+		attribute:							req.body.attribute,
+		character:							req.body.character,
+		hobby:									req.body.hobby,
+		old_min: 								req.body.old_min,
+		old_max: 								req.body.old_max,
+		religion2: 							req.body.religion2,
+		nation2: 								req.body.nation2,
+		size2_min: 							req.body.size2_min,
+		size2_max: 							req.body.size2_max,
+		weight2_min: 						req.body.weight2_min,
+		weight2_max: 						req.body.weight2_max,
+		marriage2: 							req.body.marriage2,
+		children2: 							req.body.children2,
+		svet_edication2:				req.body.svet_edication2,
+		religion_edication2:		req.body.religion_edication2,
+		observing2:							req.body.observing2,
+		attribute2:							req.body.attribute2,
+		character2:							req.body.character2,
+		dopoln:									req.body.dopoln
 	});
 	console.log(newUser);
 
@@ -64,6 +96,39 @@ router.post('/registration', (req,res,next)=>{
 		  res.redirect('/');
 		}
 	});
+});
+
+// find person
+router.post('/find-person', (req,res,next)=>{
+	console.log('find user');
+	var fp = req.body;
+
+	console.log(fp);
+
+	User.getUserAll((err, user)=>{
+		if (err) return console.log(err);
+		var filterUser = user.filter((it,index)=>{
+			// if ((fp.sex == 'Мужчину' && it.sex == 'm') || (fp.sex == 'Женщину' && it.sex == 'g'))
+			if (fp.sex == it.sex)
+				if (fp.religion == it.religion) {
+					var date = new Date(it.date);
+					it.old = calculateAge(date);
+					console.log(it.old);
+					it.dateNew = dateFormat(date);
+					if ((it.old <= fp.old_max && it.old >= fp.old_min) && (it.size <= fp.size2_max && it.size >= fp.size2_min) &&
+					    (it.weight <= fp.weight2_max && it.weight >= fp.weight2_min) && it.marriage == fp.marriage)
+					console.log('size');
+
+						if ((it.children == 0 && fp.children == 'no') || (it.children > 0 && fp.children == 'yes')) {
+							console.log(it);
+							return it;	
+						}
+				}
+			}) 
+		console.log(filterUser);
+		res.render('persons', { title: 'Анкеты участников',  person: filterUser	});
+	});
+
 });
 
 module.exports = router;
